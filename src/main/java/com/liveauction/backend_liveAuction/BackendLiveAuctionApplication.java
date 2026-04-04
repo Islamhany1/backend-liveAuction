45d6bf33b35dff6add2a87f155070d26f59799fd
Islamhany1 <94894834+Islamhany1@users.noreply.github.com>
2026-04-04 13:57:15 +0200

    Initial Spring Boot commit

---
+++ src/main/java/com/liveauction/backend_liveAuction/BackendLiveAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.liveauction.backend_liveAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLiveAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLiveAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liveauction/backend_liveAuction/BackendLiveAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveauction.backend_liveAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLiveAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
